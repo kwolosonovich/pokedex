bccabc857804e02e0b22a01022e4e49f8741ba8a
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-10-16 15:39:38 -0700

    created css file; applied styles in component

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,16 +1,17 @@
 import React from "react";
+import "./Pokecard.css"
 
 const Pokecard = ({id, name, type, baseExperience}) => {
 
     let imgUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
 
     return (
-        <div>
-            <h3>{name}</h3>
-            <img src={imgUrl} alt=""/>
+        <div className="pokecard">
+            <h3 className="pokecard-name">{name}</h3>
+            <img src={imgUrl} alt="" className="pokecard-image" />
             <ul>
-                <li>Type: {type}</li>
-                <li>EXP: {baseExperience}</li>
+                <li className="pokecard-desc">Type: {type}</li>
+                <li className="pokecard-desc">EXP: {baseExperience}</li>
             </ul>
         </div>
     )
